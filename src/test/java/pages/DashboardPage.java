01c2863c3546278372dea2345d45b372a1015f56
VaibhaviKhiranandDhadde <dhaddevaibhavi@gmail.com>
2025-01-29 16:12:30 +0530

    third commit

--- src/test/java/pages/DashboardPage.java
+++ src/test/java/pages/DashboardPage.java
@@ -1,6 +1,5 @@
 package pages;
 
-import java.time.LocalDate;
 import java.util.List;
 
 import org.openqa.selenium.By;
@@ -14,45 +13,113 @@ import base.BaseClass;
 public class DashboardPage extends BaseClass{
 	@FindBy(xpath="//img[@class='navbar-brand-img']")
 	WebElement logo;
-	
-   @FindBy(xpath="//div[@class='react-calendar']")
-   WebElement calender;
-	
+
+	@FindBy(xpath="//div[@class='react-calendar']")
+	WebElement calender;
+
+	@FindBy(xpath = "//button[contains(text(),'›')]")
+	WebElement next_month;
+
+	@FindBy(xpath="//button[contains(text(),'‹')]")
+	WebElement previous_month;
+
+	@FindBy(xpath="//span[@class='react-calendar__navigation__label__labelText react-calendar__navigation__label__labelText--from']")
+	WebElement current_month;
+
+	@FindBy(xpath="//button[normalize-space()='»']")
+	WebElement next_year;
+
+	@FindBy(xpath="//button[normalize-space()='«']")
+	WebElement previous_year;
+
+	@FindBy(xpath="//div[@class='react-calendar__month-view__days']//button")
+	WebElement month_date;
+
+	@FindBy(xpath="//span[@class='mb-0 text-sm font-weight-bold']")
+	WebElement profile_name;
+
+	@FindBy(xpath="//button[@role='menuitem']//span[contains(text(),'Logout')]")
+	WebElement btn_logout;
+
+
+
 	public DashboardPage() {
 		PageFactory.initElements(driver, this);
 	}
-	
+
 	public WebElement getLogo() {
 		return logo;
 	}
-	
+
 	public void navigationLinks(String link_text) throws InterruptedException {
 		List<WebElement> nav_links = driver.findElements(By.xpath("//ul[@class='navbar-nav']//a"));
 		for (WebElement link : nav_links) {
-            // Step 4: Check if the link's text matches the target string
-            if (link.getText().equalsIgnoreCase(link_text)) {
-                // Step 5: If it matches, click the link
-                link.click();
-                driver.getPageSource().contains(link_text);
-                 System.out.println("Clicked on the navigation link: " + link_text);
-                
-                // Exit the loop once the link has been clicked
-                break;
-            }
-        }
+			// Step 4: Check if the link's text matches the target string
+			if (link.getText().equalsIgnoreCase(link_text)) {
+				// Step 5: If it matches, click the link
+				link.click();
+				driver.getPageSource().contains(link_text);
+				System.out.println("Clicked on the navigation link: " + link_text);
+
+				// Exit the loop once the link has been clicked
+				break;
+			}
+		}
 	}
-	
-	public void calenderData() {
-		// Get today's date
-        LocalDate today = LocalDate.now();
-        String todayDay = String.valueOf(today.getDayOfMonth()); // Get today's day as string
-        String todayMonth = today.getMonth().toString().substring(0, 3).toLowerCase(); // Get today's month abbreviation (e.g., "jan")
-        String todayYear = String.valueOf(today.getYear()); // Get the current year
-        
+
+	public WebElement getNext_month() {
+		return next_month;
 	}
-	
-	
-	
-	
+
+	public void clickNextMonth() {
+		next_month.click();
+		waitImplicit();
+		System.out.println(current_month.getText());
+	}
+
+	public void clickPreviousMonth() {
+		previous_month.click();
+		waitImplicit();
+		System.out.println(current_month.getText());
+	}
+
+	public void clickNextYear() {
+		next_year.click();
+		waitImplicit();
+		System.out.println(current_month.getText());
+	}
+
+	public void clickPreviousYear() {
+		previous_year.click();
+		waitImplicit();
+		System.out.println(current_month.getText());
+	}
+
+
+	public void selectDate(String targetDay) throws InterruptedException {
+		List<WebElement> dates = driver.findElements(By.xpath("//div[@class='react-calendar__month-view__days']//button"));
+		for(WebElement date:dates)
+		{
+			if(date.getText().equals(targetDay))
+			{
+				waitUntillVisiblity(date);
+				date.click();				 
+				Assert.assertEquals(date.getText(), targetDay);
+				waitImplicit();
+				break;
+			}		
+		}
+
+	}
+
+	public void logout() {
+		profile_name.click();
+		btn_logout.click();
+	}
+
+
+
+
+
 
 }
---
+++ src/test/java/testcases/DashboardTests.java
@@ -0,0 +1,215 @@
+package testcases;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.JavascriptExecutor;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import base.BaseClass1;
+import pages.DashboardPage;
+import pages.LoginPage;
+import utilities.DataProviders;
+
+public class DashboardTests extends BaseClass1{
+	 private static final Logger logger =  LogManager.getLogger(LoginTests.class);
+	 public LoginPage login;
+	 public DashboardPage dashboard;
+	 
+	//Verify page is loaded successfully.
+		@Test(priority =1)
+		public void AECP_ProjectManager_Dashboard_TC001() throws InterruptedException {
+			logger.info("Validating page load");
+			test = extent.createTest("Verify Page load", "Ensure the page loads successfully");
+			 // Wait for the page to fully load
+	        JavascriptExecutor js = (JavascriptExecutor) driver;
+	        boolean isPageLoaded = false;
+
+	        for (int i = 0; i < 10; i++) { // Max 10 retries
+	            if (js.executeScript("return document.readyState").equals("complete")) {
+	                isPageLoaded = true;
+	                break;
+	            }
+	            Thread.sleep(1000); // Wait 1 second before checking again
+	        }
+
+	        if (isPageLoaded) {
+	            System.out.println("Page has fully loaded!");
+	        } else {
+	            System.out.println("Page load timed out.");
+	        }
+	        logger.info("Page loaded successfully with all content");
+		}
+		
+		//Verify Page title is displayed and it describes the page content	
+		@Test(priority = 2)
+		public void AECP_ProjectManager_Dashboard_TC002() throws Exception {   //PageTitle
+			
+			test = extent.createTest("Verify Page Title", "Ensure the page title matches the expected title");
+			 try {
+			String expectedTitle="AECP";
+			String actualTitle= driver.getTitle();
+			logger.info("Fetched the actual title: " + actualTitle);
+
+			Assert.assertEquals(actualTitle, expectedTitle);			
+			logger.info("actual title matches with expected title");
+			
+			test.pass("Page title verification passed.");
+			 }catch (Exception e) {
+		            logger.error("An error occurred while verifying the page title: ", e);
+		            test.fail("Page title verification failed: " + e.getMessage());
+		            throw e;  // Rethrow the exception to ensure test failure is logged
+		        }
+			
+		}
+		
+		//Verify logo is displayed.
+		@Test(priority = 3)
+		public void AECP_ProjectManager_Dashboard_TC004() {
+			logger.info("Verify logo", "Ensure logo is displayed in the page");
+			dashboard = new DashboardPage();
+			test = extent.createTest("Verify logo", "Ensure the logo is displayed in the page");
+			 try {
+			Assert.assertTrue(dashboard.getLogo().isDisplayed());
+			
+			logger.info("Logo is displayed in the page");
+			test.pass("Page logo verification passed");
+			 }catch(Exception e)
+			 {
+				 logger.error("An error occured while validating the logo:", e);
+				 test.fail("Page logo verification failed: "+e.getMessage());
+				 throw e;
+			 }
+			
+		}
+		
+		//Login to the  Project Manager Dashboard module Verify Navigation Links
+		@Test(dataProvider = "DashboardNavigation", priority = 4, dataProviderClass = DataProviders.class)
+		public void AECP_ProjectManager_Dashboard_TC008(String link_text) throws Exception {
+			logger.info("Verify navigation links are working fine");
+			dashboard = new DashboardPage();
+			test = extent.createTest("Verify navigation links", "Ensure each navigation link navigates to expected page");
+			try {
+				dashboard.navigationLinks(link_text);
+				test.pass("Each navigation link is redirecting to respective page");
+			}catch(Exception e) {
+				 logger.error("An error occured while navigating to other page:", e);
+				 test.fail("navigation link verification failed: "+e.getMessage());
+				 throw e;
+			}
+		}
+		
+		//Verify the "Next Month" button navigates to the next month in calender
+		@Test(priority = 5)
+		public void AECP_ProjectManager_Dashboard_TC016() {
+			logger.info("Verify the next month button");
+			dashboard = new DashboardPage();
+			test = extent.createTest("Verify next month button functionality");
+			try {
+				dashboard.clickNextMonth();
+				logger.info("Clicked on next month button");
+				
+				test.pass("Next month button is working fine in calendar");
+			}catch(Exception e)
+			{
+				logger.error("An error occured while navigating to next month",e);
+				test.fail("Navigation to next month failed"+e.getMessage());
+				throw e;
+			}
+		}
+		
+		//Verify the "Previous Month" button navigates to the previous month.
+		@Test(priority = 6)
+		public void AECP_ProjectManager_Dashboard_TC017() {
+			logger.info("Verify the previous month button");
+			dashboard = new DashboardPage();
+			test = extent.createTest("Verify previous month button functionality");
+			try {
+				dashboard.clickPreviousMonth();
+				logger.info("Clicked on previous month button");
+				
+				test.pass("Previous month button is working fine in calendar");
+			}catch(Exception e)
+			{
+				logger.error("An error occured while navigating to previous month",e);
+				test.fail("Navigation to previous month failed"+e.getMessage());
+				throw e;
+			}
+		}
+		
+		//Verify the "Next Year" button navigates to the next year 
+		@Test(priority = 7)
+		public void AECP_ProjectManager_Dashboard_TC018() {
+			logger.info("Verify the next year button");
+			dashboard = new DashboardPage();
+			test = extent.createTest("Verify next year button functionality");
+			try {
+				dashboard.clickNextYear();
+				logger.info("Clicked on next year button");
+				
+				test.pass("next year button is working fine in calendar");
+			}catch(Exception e)
+			{
+				logger.error("An error occured while navigating to next year",e);
+				test.fail("Navigation to next year failed"+e.getMessage());
+				throw e;
+			}
+		}
+		
+		//Verify the "Previous Year" button navigates to the previous year
+		@Test(priority = 8)
+		public void AECP_ProjectManager_Dashboard_TC019() {
+			logger.info("Verify the previous year button");
+			dashboard = new DashboardPage();
+			test = extent.createTest("Verify previous year button functionality");
+			try {
+				dashboard.clickPreviousYear();
+				logger.info("Clicked on previous year button");
+				
+				test.pass("previous year button is working fine in calendar");
+			}catch(Exception e)
+			{
+				logger.error("An error occured while navigating to previous year",e);
+				test.fail("Navigation to previous year failed"+e.getMessage());
+				throw e;
+			}
+		}
+		
+		//  Verify selecting a date highlights the selected date.
+		@Test(priority=9)
+		public void AECP_ProjectManager_Dashboard_TC020() throws Exception {
+			logger.info("Verify selecting a date from the calendar");
+			dashboard = new DashboardPage();
+			test = extent.createTest("Verify selecting a date from calendar");
+			try {
+				dashboard.selectDate("4");
+				logger.info("date is selected");
+				test.pass("Selected date is highlighted");
+			}catch(Exception e)
+			{
+				logger.error("An error occured while selecting a date from the calendar");
+				test.fail("Selecting a date from calendar failed"+e.getMessage());
+				throw e;
+			}
+		}
+		
+		//Verify that the user can log out successfully.
+		@Test(priority=10)
+		public void AECP_ProjectManager_Dashboard_TC015() {
+			logger.info("Verify logout functionality");
+			dashboard = new DashboardPage();
+			test = extent.createTest("Verify logout");
+			try {
+				logger.info("logout method initialized");
+				dashboard = new DashboardPage();
+				dashboard.logout();
+				logger.info("logged out successfully");
+				test.pass("logout functionality got pass");
+			}catch(Exception e) {
+				logger.error("An error occured while logging out");
+				test.fail("Logout functionality failed"+e.getMessage());
+				throw e;
+			}
+		}
+
+}
